607cb2dbf4f08a068652847bd426d7b5e5d8b2fe
ArunRoy404 <156922911+ArunRoy404@users.noreply.github.com>
2025-08-05 18:45:42 +0600

    conditional render by path implemented

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -1,9 +1,15 @@
+'use client'
 import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 import React from 'react';
 
 const Navbar = () => {
-    return (
-        <nav>
+    const pathname = usePathname()
+    console.log(pathname.includes('dashboard'));
+    console.log(pathname);
+
+    if (!pathname.includes('/dashboard')) {
+        return <nav>
             <ul className='flex justify-center gap-10 my-4'>
                 <Link href={'/'}>Home</Link>
                 <Link href={'/services'}>Services</Link>
@@ -11,7 +17,9 @@ const Navbar = () => {
                 <Link href={'/dashboard'}>Dashboard</Link>
             </ul>
         </nav>
-    );
+    } else {
+        return <></>
+    }
 };
 
 export default Navbar;
