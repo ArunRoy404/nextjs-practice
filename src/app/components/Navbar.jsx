96c68ebed27fa86bc77fd570ac5a913d788704e2
ArunRoy404 <156922911+ArunRoy404@users.noreply.github.com>
2025-08-05 17:31:57 +0600

    used useRouter hook for link forward

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -3,14 +3,14 @@ import React from 'react';
 
 const Navbar = () => {
     return (
-        <div>
+        <nav>
             <ul className='flex justify-center gap-10 my-4'>
                 <Link href={'/'}>Home</Link>
                 <Link href={'/services'}>Services</Link>
                 <Link href={'/about'}>About</Link>
                 <Link href={'/dashboard'}>Dashboard</Link>
             </ul>
-        </div>
+        </nav>
     );
 };
 
--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,24 @@
+'use client'
+import { useRouter } from "next/navigation";
 
 export default function Home() {
+  const router = useRouter()
+
+  const user = {
+    name: 'roy',
+    role: 'admin'
+  }
+
+  const handleGoDashboard = () => {
+    if (user.role === 'admin') {
+      router.push('/dashboard')
+    }
+  }
+
   return (
-    <div>
+    <div className="flex justify-center items-center gap-2 flex-col h-screen">
       This is home page
+      <button onClick={handleGoDashboard} className="bg-gray-300 text-black p-3 rounded-sm cursor-pointer">Go to Dashboard</button>
     </div>
   );
 }
